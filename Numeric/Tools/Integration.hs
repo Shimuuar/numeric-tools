6e095c77f6668b9964c88393ac0bded528c8b596
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2011-08-06 22:27:57 +0300

    Simpson integration

--- Numeric/Tools/Integration.hs
+++ Numeric/Tools/Integration.hs
@@ -2,28 +2,55 @@
 module Numeric.Tool.Integration (
   ) where
 
+import Debug.Trace
 import Numeric.Utils
 
+----------------------------------------------------------------
+-- Trapezoid and Simpson integraion
+----------------------------------------------------------------
+
+-- Initial guess for trapezoid rule
+trapGuess :: Double -> Double -> (Double -> Double) -> Double
+trapGuess a b f = 0.5 * (b - a) * (f b + f a)
+
+
+-- Refinement of guess using trapeziod algorithms
 nextTrapezoid :: Double             -- Lower integration limit
               -> Double             -- Upper integration limit
               -> Int                -- Number of additional points
               -> (Double -> Double) -- Function to integrate
-              -> Double             -- Correction
-nextTrapezoid from to n f = sep * (sum' $ map f $ generate n (+sep) x0)
+              -> Double             -- Approximation
+              -> Double
+nextTrapezoid a b n f q = 0.5 * (q + sep * s)
   where
-    sep = (to - from) / fromIntegral n -- Separation between points
-    x0  = from + 0.5 * sep
-
+    sep = (b - a) / fromIntegral n            -- Separation between points
+    x0  = a + 0.5 * sep                       -- Starting point
+    s   = sum' $ map f $ generate n (+sep) x0 -- Sum of all points
 
 -- | Trapezoidal integration
 quadTrapeziod :: (Double, Double)   -- ^ Integration limit
               -> Double             -- ^ Precision
               -> (Double -> Double) -- ^ Function to integrate
               -> Double
-quadTrapeziod (from,to) eps f = worker 1 (0.5 * (to - from) * (f to + f from))
+quadTrapeziod (a,b) eps f = worker 1 (trapGuess a b f)
   where
-    worker n appr 
-      | n > 2^5 && abs (appr' - appr) < eps * abs appr = appr'
-      | otherwise                                      = worker (n*2) appr'
+    worker n q
+      | n > 2^5 && abs (q' - q) < eps * abs q = q'
+      | otherwise                             = worker (n*2) q'
       where
-        appr' = 0.5 * (appr + nextTrapezoid from to n f)
+        q' = nextTrapezoid a b n f q
+-- FIXME: failure to converge?
+
+quadSimpson :: (Double, Double)   -- ^ Integration limit
+            -> Double             -- ^ Precision
+            -> (Double -> Double) -- ^ Function to integrate
+            -> Double
+quadSimpson (a,b) eps f = worker 1 0 (trapGuess a b f)
+  where
+    worker n s st 
+      | n > 2^5 && abs (s' - s) < eps * abs s = s'
+      | otherwise                             = worker (n*2) s' st'
+      where
+        st' = nextTrapezoid a b n f st
+        s'  = (4*st' - st) / 3
+-- FIXME: failure to converge?
