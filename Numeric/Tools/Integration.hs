9b88f1196e32e6cc0cc980fde48fe1a3672b2569
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2011-08-07 19:32:08 +0300

    * Handle failure to converge  * Export lists

--- Numeric/Tools/Integration.hs
+++ Numeric/Tools/Integration.hs
@@ -1,12 +1,69 @@
 {-# LANGUAGE BangPatterns #-}
 module Numeric.Tool.Integration (
+    -- * Integration parameters
+    QuadParam(..)
+  , defQuad
+    -- * Integration functions
+  , quadTrapeziod
+  , quadSimpson
   ) where
 
 import Debug.Trace
 import Numeric.Utils
 
 ----------------------------------------------------------------
--- Trapezoid and Simpson integraion
+-- Trapezoid and Simpson integration
+----------------------------------------------------------------
+
+-- | Integration parameters for numerical routines
+data QuadParam = QuadParam { 
+    quadPrecision :: Double -- ^ Relative precision of answer
+  , quadMaxIter   :: Int    -- ^ Maximum number of iterations
+  }
+  deriving (Show,Eq)
+
+-- | Default parameters for integration functions
+defQuad :: QuadParam
+defQuad =  QuadParam { quadPrecision = 1e-9
+                     , quadMaxIter   = 20
+                     }
+
+-- | Trapezoidal integration. Returns 'Nothing' if integral fails to
+--   converge
+quadTrapeziod :: (Double, Double)   -- ^ Integration limit
+              -> QuadParam          -- ^ Precision
+              -> (Double -> Double) -- ^ Function to integrate
+              -> Maybe Double
+quadTrapeziod (a,b) param f = worker 1 (trapGuess a b f)
+  where
+    eps = quadPrecision param
+    worker n q
+      | n > quadMaxIter param                 = Nothing 
+      | n > 2^5 && abs (q' - q) < eps * abs q = Just q'
+      | otherwise                             = worker (n*2) q'
+      where
+        q' = nextTrapezoid a b n f q
+
+-- | Simpson rule
+quadSimpson :: (Double, Double)   -- ^ Integration limit
+            -> QuadParam          -- ^ Precision
+            -> (Double -> Double) -- ^ Function to integrate
+            -> Maybe Double
+quadSimpson (a,b) param f = worker 1 0 (trapGuess a b f)
+  where
+    eps = quadPrecision param
+    worker n s st 
+      | n > quadMaxIter param                 = Nothing
+      | n > 2^5 && abs (s' - s) < eps * abs s = Just s'
+      | otherwise                             = worker (n*2) s' st'
+      where
+        st' = nextTrapezoid a b n f st
+        s'  = (4*st' - st) / 3
+
+-- FIXME: Romberg integration
+
+----------------------------------------------------------------
+-- Helpers
 ----------------------------------------------------------------
 
 -- Initial guess for trapezoid rule
@@ -26,31 +83,3 @@ nextTrapezoid a b n f q = 0.5 * (q + sep * s)
     sep = (b - a) / fromIntegral n            -- Separation between points
     x0  = a + 0.5 * sep                       -- Starting point
     s   = sum' $ map f $ generate n (+sep) x0 -- Sum of all points
-
--- | Trapezoidal integration
-quadTrapeziod :: (Double, Double)   -- ^ Integration limit
-              -> Double             -- ^ Precision
-              -> (Double -> Double) -- ^ Function to integrate
-              -> Double
-quadTrapeziod (a,b) eps f = worker 1 (trapGuess a b f)
-  where
-    worker n q
-      | n > 2^5 && abs (q' - q) < eps * abs q = q'
-      | otherwise                             = worker (n*2) q'
-      where
-        q' = nextTrapezoid a b n f q
--- FIXME: failure to converge?
-
-quadSimpson :: (Double, Double)   -- ^ Integration limit
-            -> Double             -- ^ Precision
-            -> (Double -> Double) -- ^ Function to integrate
-            -> Double
-quadSimpson (a,b) eps f = worker 1 0 (trapGuess a b f)
-  where
-    worker n s st 
-      | n > 2^5 && abs (s' - s) < eps * abs s = s'
-      | otherwise                             = worker (n*2) s' st'
-      where
-        st' = nextTrapezoid a b n f st
-        s'  = (4*st' - st) / 3
--- FIXME: failure to converge?
