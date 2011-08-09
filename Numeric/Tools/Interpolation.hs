db0f3571f9887f09872fb462514e23a8d3d4e793
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2011-08-09 17:24:31 +0300

    Add uniform mesh

--- Numeric/Tools/Interpolation.hs
+++ Numeric/Tools/Interpolation.hs
@@ -1,4 +1,9 @@
 module Numeric.Tool.Interpolation (
+    -- * Meshes
+    Mesh
+  , UniformMesh
+  , uniformMesh
+  , uniformMeshStep
   ) where
 
 -- | Class for one dimensional meshes 
@@ -9,7 +14,6 @@ class Mesh a where
   meshLowerBound :: a -> Double
   -- | Upper bound of mesh 
   meshUpperBound :: a -> Double
-  
   
   -- | Retrieve value at node. No check are performed
   unsafeMeshIndex :: a -> Int -> Double
@@ -26,3 +30,29 @@ class Mesh a where
 mesh <! i 
   | i < 0 || i >= meshSize mesh = error "Numeric.Tool.Interpolation.Mesh: invalid index"
   | otherwise                   = unsafeMeshIndex mesh i
+
+
+-- | Uniform mesh
+data UniformMesh = UniformMesh { uniformMeshFrom :: Double
+                               , uniformMeshStep :: Double
+                               , uniformMeshSize :: Int
+                               }
+
+-- | Create uniform mesh 
+uniformMesh :: Double           -- ^ Lower bound
+            -> Double           -- ^ Upper bound
+            -> Int              -- ^ Number of nodes
+            -> UniformMesh
+uniformMesh a b n
+  | b <= a    = error "Numeric.Tool.Interpolation.Mesh.uniformMesh: wrong range"
+  | n <  2    = error "Numeric.Tool.Interpolation.Mesh.uniformMesh: too few nodes"
+  | otherwise = UniformMesh a ((b - a) / fromIntegral (n - 1)) n
+
+instance Mesh UniformMesh where
+  meshSize            = uniformMeshSize
+  meshLowerBound      = uniformMeshFrom
+  meshUpperBound (UniformMesh a da n) = a + da * fromIntegral (n - 1)
+
+  unsafeMeshIndex (UniformMesh a da n) i = a + fromIntegral i * da
+  meshFindIndex   (UniformMesh a da n) x = truncate $ (x - a) / da
+
