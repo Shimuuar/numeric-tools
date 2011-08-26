0baaa15a63737e32004b1e9659c6859bdcfe6de1
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2011-08-26 21:28:40 +0300

    Working(?) richardson differentiation.
    It's really ugly

--- Numeric/Tools/Differentiation.hs
+++ Numeric/Tools/Differentiation.hs
@@ -5,10 +5,13 @@ module Numeric.Tool.Differentiation (
   , representableDelta 
   ) where
 
+import Control.Monad.ST (runST)
 import qualified Data.Vector.Unboxed         as U
 import qualified Data.Vector.Unboxed.Mutable as M
 
 import Numeric.FloatingPoint
+
+
 
 -- | Simplest form of differentiation. Should be used only when
 --   function evaluation is prohibitively expensive and value at point
@@ -37,12 +40,50 @@ diffSimmetric f h x = (f(x + h) - f(x - h)) / (2 * h)
     h = representableDelta x h
 
 
--- diffRichardson :: (Double -> Double) -- ^ Function
---               -> Double             -- ^ Delta
---               -> Double             -- ^ Point at which evaluate differential
---               -> Double
--- diffRichardson f h x = 0                  
+diffRichardson :: (Double -> Double) -- ^ Function
+               -> Double             -- ^ Delta
+               -> Double             -- ^ Point at which evaluate differential
+               -> Double
+diffRichardson f h x = runST $ do
+  let nMax = 10
+  let con  = 1.4
+      con2 = con*con
+  let safe = 2 
+  arr <- M.new nMax
+  -- Initial approximation
+  let d0 = (f (x + h) - f (x - h)) / (2*h)
+  M.write arr 0 d0
+  -- -- 
+  let worker i hh err ans 
+        | i == nMax = return ans
+        | otherwise = do
+            let richard j fac x err' ans'
+                  | j > i     = do
+                      M.write arr (j-1) x
+                      return (ans',err')
+                  | otherwise = do
+                      xOld <- M.read arr (j-1)
+                      M.write arr (j-1) x
+                      let x'   = (x*fac - xOld) / (fac - 1)
+                          errt = max (abs $ x' - x) (abs $ x' - xOld)
+                      if errt < err' 
+                        then richard (j+1) (fac * con2) x' errt x'
+                        else richard (j+1) (fac * con2) x' err' ans
+            -- 
+            let hh' = hh / con
+                d   = (f (x + hh') - f (x - hh')) / (2 * hh')
+            (ans',err') <- richard 1 con2 d err ans
+            x'  <- M.read arr i
+            x'' <- M.read arr i
+            if( abs (x' - x'') >= safe * err' )
+              then return ans'
+              else worker (i+1) hh' err' ans'
+  -- 
+  worker 1 h posInfty nan
 
+
+
+      
 ----------------------------------------------------------------
 
 
