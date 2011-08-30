5797946ac438ee50463e4645df68217246d6c05a
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2011-08-30 18:34:21 +0300

    Create type class for different interpolation methods

--- Numeric/Tools/Interpolation.hs
+++ Numeric/Tools/Interpolation.hs
@@ -1,61 +1,67 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts   #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TypeFamilies       #-}
 module Numeric.Tools.Interpolation (
+    -- * Type class
+    Interpolation(..)
     -- * Data types
-    InterpData
-  , interpMesh
-  , interpData
-    -- ** Constructors
-  , interpolateFromVec
-    -- *
+  , LinearInterp
+  , linearInterpMesh
+  , linearInterpTable
   ) where
 
+import Data.Data (Data,Typeable)
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Generic as G
 
 import Numeric.Classes.Indexing
 import Numeric.Tools.Mesh
 
 ----------------------------------------------------------------
 
--- | Table for interpolation
-data InterpData a = InterpData { interpMesh :: a
-                               , interpData :: U.Vector Double
-                               }
-                    deriving (Show,Eq)
-
-instance Mesh a => Indexable (InterpData a) where
-  type IndexVal (InterpData a) = (IndexVal a, Double)
-  size        (InterpData _    vec)   = size vec
-  unsafeIndex (InterpData mesh vec) i = ( unsafeIndex mesh i
-                                        , unsafeIndex vec  i
-                                        )
-
-  
--- | Create table from mesh and vector of values.
-interpolateFromVec :: Mesh a => a -> U.Vector Double -> InterpData a
-interpolateFromVec mesh vec 
-  | size mesh /= size vec = error "Numeric.Tools.Interpolation.interpolateFromVec: size of vector and mesh do not match"
-  | otherwise             = InterpData mesh vec
-
+-- | Interpolation for arbitraty meshes
+class Interpolation a where
+  -- | Interpolate function at some point. Interpolation should not
+  --   fail outside of mesh however it may and will give nonsensical
+  --   results
+  at          :: (IndexVal m ~ Double, Mesh m) => a m -> Double -> Double
+  -- | Tabulate function
+  tabulateFun :: (IndexVal m ~ Double, Mesh m) => m -> (Double -> Double) -> a m
+  -- | Use table of already evaluated function and mesh. Sizes of mesh
+  --   and table must coincide
+  tabulate    :: (IndexVal m ~ Double, Mesh m, G.Vector v Double) => m -> v Double -> a m
 
 ----------------------------------------------------------------
 
--- | Linear interpolation. If value is out of range throw exception
-linearInterpolation :: (Mesh a, IndexVal a ~ Double) => InterpData a -> Double -> Double
-linearInterpolation tbl@(InterpData mesh _) x
-  | validIndex mesh i = workerLinearInterpolation tbl i x
-  | otherwise         = error "Numeric.Tools.Interpolation.linearInterpolation: value is out of range"
-  where
-    i = meshFindIndex mesh x
-  
--- | Linear interpolation
-unsafeLinearInterpolation :: (Mesh a, IndexVal a ~ Double) => InterpData a -> Double -> Double
-unsafeLinearInterpolation tbl@(InterpData mesh _) x =
-  workerLinearInterpolation tbl (meshFindIndex mesh x) x
+-- | Data for linear interpolation
+data LinearInterp a = LinearInterp { linearInterpMesh  :: a
+                                   , linearInterpTable :: U.Vector Double
+                                   }
+                      deriving (Show,Eq,Data,Typeable)
 
+instance Mesh a => Indexable (LinearInterp a) where
+  type IndexVal (LinearInterp a) = (IndexVal a, Double)
+  size        (LinearInterp _    vec)   = size vec
+  unsafeIndex (LinearInterp mesh vec) i = ( unsafeIndex mesh i
+                                          , unsafeIndex vec  i
+                                          )
+  {-# INLINE size        #-}
+  {-# INLINE unsafeIndex #-}
 
--- Worker function for linear interpolation
-workerLinearInterpolation :: (Mesh a, IndexVal a ~ Double) => InterpData a -> Int -> Double -> Double
-workerLinearInterpolation tbl i x = a + (x - xa) / (xb - xa) * (b - a)
+instance Interpolation LinearInterp where
+  at = linearInterpolation
+  tabulate mesh tbl
+    | size mesh /= G.length tbl = error "Numeric.Tools.Interpolation.LinearInterp.tabulate: size of vector and mesh do not match"
+    | otherwise                 = LinearInterp mesh (G.convert tbl)
+  tabulateFun mesh f = LinearInterp mesh (U.generate (size mesh) (f . unsafeIndex mesh))
+
+linearInterpolation :: (Mesh a, IndexVal a ~ Double) => LinearInterp a -> Double -> Double
+linearInterpolation tbl@(LinearInterp mesh _) x = a + (x - xa) / (xb - xa) * (b - a)
   where
+    n  = size mesh - 2
+    i  = meshFindIndex mesh x
+    i' | i < 0     = 0
+       | i > n     = n
+       | otherwise = i
     (xa,a) = unsafeIndex tbl  i
     (xb,b) = unsafeIndex tbl (i+1)
