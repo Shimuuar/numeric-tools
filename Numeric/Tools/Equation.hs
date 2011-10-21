7ddba57ff3ada0ecfbd4755dc4b0cf8633e6c238
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2011-10-21 12:03:05 +0300

    Add special return type for equation solvers.
    Ported from statistics

--- Numeric/Tools/Equation.hs
+++ Numeric/Tools/Equation.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 -- |
 -- Module    : Numeric.Tools.Equation
 -- Copyright : (c) 2011 Aleksey Khudyakov
@@ -9,11 +10,64 @@
 --
 -- Numerical solution of ordinary equations.
 module Numeric.Tools.Equation ( 
-    solveBisection
+    -- * Data type
+    Root(..)
+  , fromRoot
+    -- * Equations solversv
+  , solveBisection
   ) where
 
-import Numeric.IEEE (epsilon)
+import Numeric.IEEE        (epsilon)
 
+import Control.Applicative
+import Control.Monad       (MonadPlus(..), ap)
+import Data.Typeable       (Typeable)
+
+
+
+-- | The result of searching for a root of a mathematical function.
+data Root a = NotBracketed
+            -- ^ The function does not have opposite signs when
+            -- evaluated at the lower and upper bounds of the search.
+            | SearchFailed
+            -- ^ The search failed to converge to within the given
+            -- error tolerance after the given number of iterations.
+            | Root a
+            -- ^ A root was successfully found.
+              deriving (Eq, Read, Show, Typeable)
+
+instance Functor Root where
+  fmap _ NotBracketed = NotBracketed
+  fmap _ SearchFailed = SearchFailed
+  fmap f (Root a)     = Root (f a)
+
+instance Monad Root where
+  NotBracketed >>= _ = NotBracketed
+  SearchFailed >>= _ = SearchFailed
+  Root a       >>= m = m a
+  return = Root
+
+instance MonadPlus Root where
+  mzero = SearchFailed
+  r@(Root _) `mplus` _ = r
+  _          `mplus` p = p
+
+instance Applicative Root where
+  pure  = Root
+  (<*>) = ap
+
+instance Alternative Root where
+  empty = SearchFailed
+  r@(Root _) <|> _ = r
+  _          <|> p = p
+
+-- | Returns either the result of a search for a root, or the default
+-- value if the search failed.
+fromRoot :: a                   -- ^ Default value.
+         -> Root a              -- ^ Result of search for a root.
+         -> a
+fromRoot _ (Root a) = a
+fromRoot a _        = a
 
 
 -- | Solve equation @f(x) = 0@ using bisection method. Function is
@@ -23,11 +77,10 @@ import Numeric.IEEE (epsilon)
 solveBisection :: Double             -- ^ Required absolute precision
                -> (Double,Double)    -- ^ Range
                -> (Double -> Double) -- ^ Equation
-               -> Maybe Double
+               -> Root Double
 solveBisection eps (a,b) f
-  | a >= b      = Nothing
-  | fa * fb > 0 = Nothing
-  | otherwise   = Just $ bisectionWorker (abs eps) f a b fa fb
+  | fa * fb > 0 = NotBracketed
+  | otherwise   = Root $ bisectionWorker (abs eps) f a b fa fb
   where
     fa = f a
     fb = f b
